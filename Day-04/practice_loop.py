86bf98e37a306dc92f1b7e1fdbccdb26f0e367ea
Dharmil Panchal <dharmil132@gmail.com>
2025-08-12 21:36:23 +0530

    Added new pattern of loop in  practice_loop.py

--- Day-04/practice_loop.py
+++ Day-04/practice_loop.py
@@ -71,3 +71,14 @@ for i in range(1, n+1):
         for k in range(1,(n-i+1)*2):
             print("*",end='')
         print()
+
+
+print("\nH-2. Pascal's Triangle")
+
+for i in range(1, n+1):
+    print(' '*(n-i), end='')
+    val = 1
+    for j in range(1, i+1):
+        print(val, end=' ')
+        val = val * (i - j) // j
+    print()
